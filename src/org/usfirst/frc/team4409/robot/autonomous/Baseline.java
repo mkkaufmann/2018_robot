b8952514b594827b5b016ceff40a1e0fb217b3c2
mkkaufmann <mktrash422@gmail.com>
2018-02-12 16:17:24 -0500

    test

--- src/org/usfirst/frc/team4409/robot/autonomous/Baseline.java
+++ src/org/usfirst/frc/team4409/robot/autonomous/Baseline.java
@@ -6,8 +6,6 @@ public class Baseline extends Autonomous{
 	
 	public Baseline() {
 		super();
-		commands.add(new DriveCommand(360,360,0.35));
-		commands.add(new DriveCommand(200,200,-0.35));
-		//commands.add(new DriveCommand(0,400,-0.35));
+		commands.add(new DriveCommand(350,350,0.35));
 	}
 }
---
+++ src/org/usfirst/frc/team4409/robot/autonomous/commands/LiftCommand.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team4409.robot.autonomous.commands;
+
+import org.usfirst.frc.team4409.robot.Robot;
+import org.usfirst.frc.team4409.robot.RobotMap;
+import org.usfirst.frc.team4409.robot.subsystems.Elevator;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Talon;
+
+public class LiftCommand extends AutonomousCommand{
+	
+	private static Encoder liftEnc = RobotMap.liftEnc;
+	private static Talon elevatorLeft = RobotMap.elevatorLeft;
+	private static Talon elevatorRight = RobotMap.elevatorRight;
+	private double encGoal;
+	private double power;
+	
+	public LiftCommand(double _amount,double _power) {
+		encGoal = _amount;
+		power = _power;
+	}
+	
+	
+	@Override
+	public boolean Run(){
+		double speed = 0;
+		if(Math.abs(liftEnc.getDistance()*RobotMap.EncScale)<encGoal){
+			speed = power;
+		}
+		if(Math.abs(liftEnc.getDistance()*RobotMap.EncScale)<encGoal){
+			speed = power;
+		}
+		
+		if(speed == 0){
+			return true;
+		}else{
+			elevatorLeft.set(-speed);
+			elevatorRight.set(speed);
+			return false;
+		}
+	}
+}
